6f94d2451b7890fd8ea8474eac7422126319040c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-31 23:03:07 +0300

    Create A-02-Comments.cpp

---
+++ A-02-Comments.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    //Single-line comment... 
+
+    /*
+    
+    Multi-line comment area!
+
+    Mustafa Buyukdereli 2024
+
+    Everything comes to him who waits...
+    
+    */
+}
