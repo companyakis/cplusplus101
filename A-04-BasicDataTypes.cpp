8efde814932779d3506fedb87bfc2a5e78d6c7b7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-01 00:10:26 +0300

    Create A-04-BasicDataTypes.cpp

---
+++ A-04-BasicDataTypes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string myName = "Mustafa";
+
+	int thisYear = 2024;
+
+	float average = 22.14f;
+
+	double ethBalance = 2.121458798888881177;
+
+	bool isCompleted = false;
+
+	char conditionCharacter = 'A'; 
+
+	//We can use 65 (ASCII)
+
+	char conditionCharacter2 = 65;
+
+	cout << conditionCharacter << endl; // A
+
+	cout << conditionCharacter2 << endl; // A
+	
+}
