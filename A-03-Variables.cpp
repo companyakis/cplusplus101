ee950e094c76d30041abeda1173dd120fe3932d5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-01-31 23:38:00 +0300

    Create A-03-Variables.cpp

---
+++ A-03-Variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string myName; //declaration
+
+	myName = "Mustafa"; //initialization
+
+	int thisYear = 2024;
+
+	string language = "C++";
+
+	cout << myName << " is here and year " << thisYear << " will be a good year!" << endl;
+
+	cout << "Why " << language << "? I think " << language << " is good for algorithmic trading and smart contract development..." << endl;
+
+  /*
+Mustafa is here and year 2024 will be a good year!
+Why C++? I think C++ is good for algorithmic trading and smart contract development...
+  
+  */
+
+}
