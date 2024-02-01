925532c8240e627e6e4403d813cf40b6d62048ed
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-01 21:53:21 +0300

    Create A-06-UserInput.cpp

---
+++ A-06-UserInput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int number1;
+	int number2;
+	int sum;
+
+	cout << "Number 1: ";
+	cin >> number1;
+	cout << "Number 2: ";
+	cin >> number2;
+
+	sum = number1 + number2;
+
+	cout << "Number1 + Number2 = " << sum << endl;
+
+	return 0;
+
+}
