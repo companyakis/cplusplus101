7f00d8ea3a7a3ea93696d096dd25fee5738b5287
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-05 11:47:55 +0300

    Create A-07-IfElse01.cpp

---
+++ A-07-IfElse01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	bool areYouReadyToLearnCPlusPlus = true;
+
+	if (areYouReadyToLearnCPlusPlus) {
+		cout << "C++ is old, but important... Algorithmic trading and smart contract!..";
+	}
+	else {
+		cout << "You can learn Rust, Golang etc...";
+	}
+	return 0;
+}
