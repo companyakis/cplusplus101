6745070509faeb06f9294c1a7e02b9572594a870
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-05 11:50:36 +0300

    Create A-07-IfElse02.cpp

---
+++ A-07-IfElse02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int ethBalance = 3;
+
+	if (ethBalance > 10) {
+		cout << "You should be happy:)";
+	}
+	else {
+		cout << "Be patient:(";
+	}
+	return 0;
+}
