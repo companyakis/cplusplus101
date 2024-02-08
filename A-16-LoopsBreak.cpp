005d435f992f61abc36a8d74d3571a3274e26cac
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 10:21:27 +0300

    Create A-16-LoopsBreak.cpp

---
+++ A-16-LoopsBreak.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	for (int i = 0; i <= 15; i = i + 2) {
+		if (i % 11 == 1) {
+			break;
+		}
+		cout << i << " ";
+	}
+}
+
+/*
+0 2 4 6 8 10
+*/
