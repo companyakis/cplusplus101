6a6678d0c0eed5e42e6add50b59cf3f424f1a044
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 11:36:00 +0300

    Create B-02-FunctionsMultipleParameters.cpp

---
+++ B-02-FunctionsMultipleParameters.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+//multiple parameters
+
+int areaOfRectangular(int a, int b) {
+	return a * b;
+}
+
+int main() {
+
+	//let's calculate area of a rectangular 12 * 17
+
+	cout << "Area of our rectangular: " << areaOfRectangular(12, 17) << endl;
+
+}
+
+/*
+Area of our rectangular: 204
+*/
