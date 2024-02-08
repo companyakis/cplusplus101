5d27b0c4954d21e7d8391eb4655498bc3e596da5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 11:44:07 +0300

    Create B-03-VoidFunctions.cpp

---
+++ B-03-VoidFunctions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+//void functions
+
+void areaOfRectangular(int a, int b) {
+
+	cout << "Area of our rectangular: " << a * b << endl;
+}
+
+int main() {
+
+	//let's call the void function two times
+
+	areaOfRectangular(11, 10);
+
+	areaOfRectangular(15, 14);
+
+}
+
+/*
+Area of our rectangular: 110
+Area of our rectangular: 210
+*/
