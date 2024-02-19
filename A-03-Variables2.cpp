3ad97d56f3c344e0a30e24929d1c5ea1b1806c75
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-19 14:53:35 +0300

    Create A-03-Variables2.cpp

---
+++ A-03-Variables2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>    
+
+using namespace std;
+
+int main()
+
+{  
+	//constants
+
+	const double piNumber = 3.14;
+
+	const string myName = "Mustafa";
+
+	cout << "These constant values can't be changed: " << piNumber << " - " << myName;
+
+	return 0;
+}
+
+
+/*
+
+These constant values can't be changed: 3.14 - Mustafa
+
+*/
